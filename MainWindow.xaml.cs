1a5eb0280dd4c6b9ef7e1508bc2db1e794ac582b
Michał Gronau <silentmichal@wp.pl>
2021-05-12 23:47:12 +0200

    Dodaj pliki projektów.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SMOLS2000
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OpenFile audioFile;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            audioFile = new OpenFile();
+            int aaaaa = audioFile.testowanko();
+            System.Console.WriteLine(aaaaa);
+            
+        }
+    }
+}
---
+++ OpenFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SMOLS2000
+{
+    class OpenFile
+    {
+        int test = -1;
+        public OpenFile()
+        {
+            MessageBox.Show("Udało Ci się kliknąć przycisk. 1 punkt dla KSEMWETIPG.", "SUKCES", MessageBoxButton.OK, MessageBoxImage.Information);
+
+        }
+
+        public int testowanko()
+        {
+            return test;
+        }
+
+    }
+}
